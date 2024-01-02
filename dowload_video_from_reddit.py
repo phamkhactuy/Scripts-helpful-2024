eb9fc45120a5b0896f5ca2eee791938eba80702c
PKT_ZZ <pktsspktss@gmail.com>
2024-01-02 13:52:09 +0700

    Update dowload_video_from_reddit.py

--- dowload_video_from_reddit.py
+++ dowload_video_from_reddit.py
@@ -3,4 +3,4 @@ from redvid import Downloader
 reddit = Downloader(max_q=True)
 reddit.url = 'https://www.reddit.com/r/Whatcouldgowrong/comments/iaf07q/put_an_electric_cable_behind_the_grapes/'
 reddit.download()
-
+#Doan code tai vieo tu URL Reddit
